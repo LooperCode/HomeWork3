94798a8cda92f26ba8cbdffe7f46837585129938
Denis <Savchenkodkz@gmail.com>
2022-11-30 23:43:04 +0600

    change

--- Program.cs
+++ Program.cs
@@ -1,48 +1,21 @@
-﻿ int[] stem = {20, 25, 10, 15, 8, 25, 11};
-int size = stem.Length;
-int index = 0;
-int max = (stem[index] + stem[index +1]) + stem[index +2];
-int max2 = (stem[size-2] + stem[size -1]) + stem[0];
-int max3 = (stem[size -1] + stem[0]) + stem [1];
-int total_max = 0;
-while (index <= size - 3){
-       int TempSum = (stem[index] + stem[index +1]) + stem[index +2];
-    if (TempSum > max){
-        max = TempSum;
-        index ++; }
-    else {
-        index ++;
-    }    
-if (max > max2 && max > max3){
-    total_max = max;
+﻿//Определение диапозона координат по вводу четверти координатной плоскости
+Console.WriteLine("Введите четверть плоскости");
+double insert = Convert.ToDouble(Console.ReadLine());
+if (insert == 1){
+    Console.WriteLine("Возможный диапозон координат: x > 0, y > 0");
 }
-else if (max2 > max && max2 > max3) {
-    total_max = max2;
+else if (insert == 2){
+    Console.WriteLine("Возможный диапозон координат: x < 0, y > 0");
 }
-else  {
-    total_max = max3;
+else if (insert == 3){
+    Console.WriteLine("Возможный диапозон координат: x < 0, y < 0");
 }
-    
-       }
-    Console.WriteLine(total_max);
-
-/*int[] stem = {6, 25, 10, 6, 8, 10, 30};
-int size = stem.Length;
-int maxSum = 0;
-for (int i = 0; i < size; i++) {
-    int sum = stem[i % size] + stem[(i +1)%size] + stem[(i +2) % size];
-if (sum > maxSum){
-    maxSum = sum;
+else if (insert == 4){
+    Console.WriteLine("Возможный диапозон координат: x > 0, y < 0");
 }
+else {
+    Console.WriteLine("Вы ввели не корректное значение");
 }
-Console.WriteLine(maxSum);
-
-*/
-
-
-
-
-
 
 
 
