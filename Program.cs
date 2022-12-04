0491a60c63c136f4a3fbb7a57c0c04089472ab4d
Denis <Savchenkodkz@gmail.com>
2022-12-05 04:29:19 +0600

    Задача 19

--- Program.cs
+++ Program.cs
@@ -1,23 +1,29 @@
-﻿//Определение диапозона координат по вводу четверти координатной плоскости
-Console.WriteLine("Введите четверть плоскости");
-double insert = Convert.ToDouble(Console.ReadLine());
-if (insert == 1){
-    Console.WriteLine("Возможный диапозон координат: x > 0, y > 0");
+﻿Console.WriteLine("Введите пятизначное число");
+int insert = Convert.ToInt32(Console.ReadLine());
+int thousand = insert;
+int last = insert % 100;
+if (insert > 99999) {
+    Console.WriteLine("Некорректное число");
 }
-else if (insert == 2){
-    Console.WriteLine("Возможный диапозон координат: x < 0, y > 0");
-}
-else if (insert == 3){
-    Console.WriteLine("Возможный диапозон координат: x < 0, y < 0");
-}
-else if (insert == 4){
-    Console.WriteLine("Возможный диапозон координат: x > 0, y < 0");
+else if (insert < 9999) {
+    Console.WriteLine("Некорректное число");
 }
 else {
-    Console.WriteLine("Вы ввели не корректное значение");
+while (thousand > 999) {
+    
+    thousand = thousand / 100;
 }
-
-
-
+thousand = (thousand % 10) * 100 + (insert / 1000) % 10 * 10 + (insert / 10000);
+last = (last % 10) * 10 + (last / 10);
+last = last * 1000;
+int reverse = last + thousand;
+if (insert == reverse) {
+    Console.WriteLine ($"Число {insert} является палиндромом, потому что в обратном порядке: {reverse}");
+}
+else {
+    Console.WriteLine($"Число {insert} не является палиндромом, потому что в обратном порядке: {reverse}");
+}
+}
+ 
 
 
